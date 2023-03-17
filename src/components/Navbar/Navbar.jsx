d9c476074d9c5eda74462308d05a8f1e28f05ecf
StuartMolnar <stuartmolnar@hotmail.com>
2023-03-17 04:17:30 -0700

    update favicon and structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
+import React from 'react';
 import './navbar.css'
+
+// !!! don't use hidden div-bars for spacing, use margin or padding
 
 function Navbar() {
 
   return (
     
     <div className="Navbar">
-      <h2 className="font-poppins font-semibold text-section-header-lg text-gold">Section Header (40px)</h2>
+      <h2 className="font-poppins font-semibold text-section-header-lg text-gold bg-black">Section Header (40px)</h2>
 
     </div>
 
-      /* <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://reactjs.org" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-    </div>
-    <h1>Vite + React</h1>
-    <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
-      </p>
-    </div>
-    <p className="read-the-docs">
-      Click on the Vite and React logos to learn more
-    </p> */
   )
 }
 
