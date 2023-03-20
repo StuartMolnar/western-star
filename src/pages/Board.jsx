5011e7db6b4388f091c02d9f425756d815174f6c
StuartMolnar <stuartmolnar@hotmail.com>
2023-03-20 04:24:06 -0700

    opacity board member card

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -66,8 +66,8 @@ const Board = () => {
           <div ref={boardMembersRef} className="board-members p-20 flex justify-center">
             <div className="grid grid-cols-2 grid-rows-2 gap-4 p-4 justify-center">
               {boardMembers.map((member, index) => (
-                <div key={index} className="bg-black p-4 rounded-2xl shadow max-w-[600px]">
-                  <div className="card-content p-8 flex flex-col h-full justify-between">
+                <div key={index} className="bg-black p-4 rounded-2xl opacity-90 shadow max-w-[600px]">
+                  <div className="card-content p-8 flex flex-col h-full justify-between opacity-150">
                     <div>
                       <h3 className="text-card-header font-bold">{member.name}</h3>
                       <p className="text-card-subheader font-bold text-gold mt-2">{member.role}</p>
