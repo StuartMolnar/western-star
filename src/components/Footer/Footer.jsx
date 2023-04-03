b87ba775dbb39dad3e3abcdaec752dd8b876f9e6
StuartMolnar <stuartmolnar@hotmail.com>
2023-04-02 22:33:56 -0700

    footer mobile version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,11 +9,16 @@ const Footer = () => {
   const isSpecialPage = location.pathname === '/overview' || location.pathname === '/ws-project';
 
   return (
-    <footer id="footer-container" className={`footer-container px-32 text-copyright relative overflow-hidden ${isSpecialPage ? 'pt-72' : ''}`}>
+    <footer id="footer-container" className={`footer-container px-12 dsk:px-32 text-copyright relative overflow-hidden ${isSpecialPage ? 'pt-72' : ''}`}>
 
-    <div className="footer-content w-full mb-8 mt-32">
+    <Link to="/" className="nav-logo h-7 items-center w-[265px] space-x-4 flex dsk:hidden mb-8 mt-24">
+      <img src="/assets/logo/ws-icon.svg" alt="icon" className="w-[55px]" />
+      <img src="/assets/logo/ws-logo.svg" alt="logo" className="w-[210px] h-[24px]" />
+    </Link>
+
+    <div className="footer-content w-full mb-8 dsk:mt-32 mt-12">
       <div className="grid grid-cols-2 h-full">
-        <div className="grid grid-cols-2 grid-rows-2 h-full col-span-1">
+        <div className="dsk:grid dsk:grid-cols-2 grid-rows-2 h-full col-span-2 dsk:col-span-1 justify-center dsk:space-y-0 space-y-8">
           <div className="flex-row items-center p-4 space-y-4">
             <span className="flex"><img src="\assets\footer\mail-icon.svg" alt="email_icon" className="mr-4" /><p className="font-medium">Email</p></span>
             <p><a href="mailto:info@riominerals.com">info@riominerals.com</a></p>
@@ -43,7 +48,7 @@ const Footer = () => {
       </div>
     </div>
     
-    <div className="footer-images">
+    <div className="footer-images hidden dsk:block">
       <div className={`gradient-gray absolute ${isSpecialPage ? 'top-[20%]' : 'top-0'} left-1/2 h-4/5 w-1/4 scale-150`}></div>
       <div className={`gradient-gold absolute ${isSpecialPage ? 'top-[45%]' : 'top-[25%]'} left-3/4 h-4/5 w-1/4 scale-150`}></div>
 
@@ -55,7 +60,7 @@ const Footer = () => {
 
     <div className="footer-copyright w-full my-16">
       <div className="flex justify-between items-center">
-        <Link to="/" className="nav-logo flex h-7 items-center w-[265px] space-x-4">
+        <Link to="/" className="nav-logo h-7 items-center w-[265px] space-x-4 hidden dsk:flex">
           <img src="/assets/logo/ws-icon.svg" alt="icon" className="w-[55px]" />
           <img src="/assets/logo/ws-logo.svg" alt="logo" className="w-[210px] h-[24px]" />
         </Link>
