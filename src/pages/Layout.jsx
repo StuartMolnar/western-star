e0b8f7fbbcb2cfffd27fc7da02a43cea31006510
StuartMolnar <stuartmolnar@hotmail.com>
2023-04-17 17:02:30 -0700

    update meta descriptions

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -21,6 +21,20 @@ function getTitleForPath(path) {
   return titleMap[path] || "Western Star Resources";
 }
 
+function getDescriptionForPath(path) {
+  const descriptionMap = {
+    "/": "Western Star is a junior Canadian exploration company with 100% ownership of the high-grade Western Star Project in the Revelstoke Mining District, British Columbia, Canada. We are committed to increasing shareholder value through cost-effective exploration practices, acquisition of high-value properties, and partnerships with industry leaders.",
+    "/overview": "Discover Western Star Resources, a junior Canadian exploration company with 100% ownership of the Western Star Project in the Revelstoke Mining District, British Columbia. Learn about our strategic goals, commitment to responsible exploration, and the advantages of mining in British Columbia.",
+    "/board": "Meet the experienced and dedicated board members of Western Star Resources, who guide our strategic vision and drive the company's success in the mining industry. Learn about their backgrounds, expertise, and commitment to responsible and sustainable exploration.",
+    "/ws-project": "Discover the Western Star Project, an exciting gold exploration opportunity with rich mineralization, located in a geologically favorable area in Southeastern British Columbia. Learn about the project's history, geology, and ongoing exploration activities.",
+    "/investors": "Explore investment opportunities with Western Star Resources, a junior Canadian exploration company. Discover our share structure, warrants outstanding, and fully diluted shares. Access our corporate presentation to gain insights into our strategy, goals, and financial performance.",
+    "/news": "Stay up-to-date with the latest news, announcements, and updates from Western Star Resources. Discover insights about the company's operations, projects, and industry developments that may impact its performance.",
+    "/legal": "Learn about Western Star Resources' legal and privacy policies, including forward-looking statements, investment decisions, limitation of liability, and QP statement. Understand the risks, uncertainties, and factors that may affect the company's performance and operations.",
+    "/western-star-plans-large-program": "Stay updated on Western Star's 2023 work program as they prepare to launch an ambitious exploration initiative in the mineral-rich Southeastern British Columbia region. Check back for the latest news and developments.",
+  }
+  return descriptionMap[path] || "Western Star is a junior Canadian exploration company with 100% ownership of the high-grade Western Star Project in the Revelstoke Mining District, British Columbia, Canada. We are committed to increasing shareholder value through cost-effective exploration practices, acquisition of high-value properties, and partnerships with industry leaders.";
+}
+
 const Layout = ({ children, setNavbarHeight }) => {
 
   const location = useLocation();
@@ -33,11 +47,16 @@ const Layout = ({ children, setNavbarHeight }) => {
 
     return `${basePath}${path}`;
   };
+
+  const getDescription = () => {
+    return getDescriptionForPath(location.pathname);
+  }
   
   return (
     <div className="layout-container">
       <Helmet>
         <link rel="canonical" href={getCanonicalUrl()} />
+        <meta name="description" content={getDescription()} />
       </Helmet>
       <Navbar setNavbarHeight={setNavbarHeight} />
       <div className="layout-content">{children}</div>
