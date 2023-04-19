479af15b9f539529aa63bdb37cd17d0a4a3762d4
StuartMolnar <stuartmolnar@hotmail.com>
2023-04-19 15:41:08 -0700

    update soft 404 SEO issue

--- src/pages/articles/Article-1.jsx
+++ src/pages/articles/Article-1.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import Layout from '../Layout';
 import '../../styles/articles/article-1.css';
 import ArticleHeader from '../../components/Article/Article-Header';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 
 import articles from '../../data/articles.json';
@@ -17,20 +18,27 @@ const Article_1 = () => {
 
 
   return (
-    <Layout setNavbarHeight={setNavbarHeight}>
+    <>    
+      <HelmetProvider>
+          <Helmet>
+          <meta name="robots" content="noindex"></meta>
+          </Helmet>
+      </HelmetProvider>
+      <Layout setNavbarHeight={setNavbarHeight}>
 
-      <ArticleHeader navbarHeight={navbarHeight} article={article} />
+        <ArticleHeader navbarHeight={navbarHeight} article={article} />
 
-      {/* content section */}
-      <section id="article-content" className="article-content px-12 dsk:px-32 bg-white text-black">
-        <h2 className="text-section-header-lg font-semibold pt-12">
-          Coming Soon
-        </h2>
-        <p className="py-12">
-          This article is currently under construction. Please check back later.
-        </p>
-      </section>
-    </Layout>
+        {/* content section */}
+        <section id="article-content" className="article-content px-12 dsk:px-32 bg-white text-black">
+          <h2 className="text-section-header-lg font-semibold pt-12">
+            Coming Soon
+          </h2>
+          <p className="py-12">
+            This article is currently under construction. Please check back later.
+          </p>
+        </section>
+      </Layout>
+    </>
   );
 };
 
